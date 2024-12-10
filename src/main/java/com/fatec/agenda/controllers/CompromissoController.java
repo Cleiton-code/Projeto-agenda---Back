a2f4b1d31ab4a275d28d4750d37e57c5f162eba6
cleiton valentim <cleiton_valentim@hotmail.com.br>
2024-12-10 00:48:21 -0300

    Finish

---
+++ src/main/java/com/fatec/agenda/controllers/CompromissoController.java
@@ -0,0 +1,63 @@
+package com.fatec.agenda.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fatec.agenda.dtos.CompromissoRequest;
+import com.fatec.agenda.dtos.CompromissoResponse;
+import com.fatec.agenda.services.CompromissoService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("compromisso")
+public class CompromissoController {
+    
+    @Autowired
+    private CompromissoService service;
+
+    @GetMapping
+    public ResponseEntity<List<CompromissoResponse>> GetCompromisso() {
+    		return ResponseEntity.ok(service.getAllCompromisso());
+}
+
+    @GetMapping("{id}")
+    public ResponseEntity<CompromissoResponse> GetCompromisso(@PathVariable Long id){
+        return ResponseEntity.ok(service.GetCompromissoById(id));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> DeleteCompromisso(@PathVariable long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> UpdateCompromisso(@PathVariable long id, @Validated @RequestBody CompromissoRequest compromisso){
+        service.update(compromisso, id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping()
+    public ResponseEntity<CompromissoResponse> SaveCompromisso(@Validated @RequestBody CompromissoRequest compromisso){
+        System.out.println("Payload Recebido: " + compromisso);
+        CompromissoResponse newCompromisso = service.save(compromisso);
+        return ResponseEntity.created(null).body(newCompromisso);
+    }
+
+
+
+    
+}
--- src/main/java/com/fatec/agenda/services/CompromissoService.java
+++ src/main/java/com/fatec/agenda/services/CompromissoService.java
@@ -6,9 +6,13 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fatec.agenda.dtos.CompromissoRequest;
 import com.fatec.agenda.dtos.CompromissoResponse;
+import com.fatec.agenda.entities.Compromisso;
 import com.fatec.agenda.mappers.CompromissoMapper;
 import com.fatec.agenda.repositories.CompromissoRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CompromissoService {
@@ -18,5 +22,41 @@ public class CompromissoService {
 
     public List<CompromissoResponse> getAllCompromisso() {
 return repository.findAll().stream().map(p -> CompromissoMapper.toDTO(p)).collect(Collectors.toList());
+    }
+
+    public CompromissoResponse GetCompromissoById(long id){
+        Compromisso compromisso = repository.findById(id).orElseThrow(
+            () -> new EntityNotFoundException("Compromisso não cadastrado")
+        );
+
+        return CompromissoMapper.toDTO(compromisso);
+    }
+
+    public void delete(long id){
+        if (repository.existsById(id)) {
+            
+        repository.deleteById(id);
+    }else{
+        throw new EntityNotFoundException("Compromisso não encontrado");
+    }
+    }
+
+    public CompromissoResponse save(CompromissoRequest compromisso){
+        Compromisso newCompromisso = repository.save(CompromissoMapper.toEntity(compromisso));
+        return CompromissoMapper.toDTO(newCompromisso);
+    }
+
+    public void update(CompromissoRequest compromisso,long id){
+        Compromisso aux = repository.getReferenceById(id);
+
+        aux.setTitle(compromisso.title());
+        aux.setDescricao(compromisso.descricao());
+        aux.setDatacompromisso(compromisso.datacompromisso());
+        aux.setInicio(compromisso.inicio());
+        aux.setFim(compromisso.fim());
+        aux.setLocal(compromisso.local());
+        aux.setSituacao(compromisso.situacao());
+
+        repository.save(aux);
     }
 }
